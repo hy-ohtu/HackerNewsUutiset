c3b91fce70bb58e232c1ce16bcfb6a6cd085429d
Miika Hänninen <miika.hanninen@gmail.com>
2015-09-08 20:26:58 +0300

    Pilkkoo HackerPaivanUutiset-luokan metodit järkeviin palasiin

--- src/main/java/com/mycompany/hackernewsuutiset/HackerPaivanUutiset.java
+++ src/main/java/com/mycompany/hackernewsuutiset/HackerPaivanUutiset.java
@@ -3,6 +3,7 @@ package com.mycompany.hackernewsuutiset;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import com.mycompany.paivanuutiset.PaivanUutiset;
 
 import java.lang.reflect.Array;
@@ -11,29 +12,48 @@ import java.util.Arrays;
 public class HackerPaivanUutiset implements PaivanUutiset {
     
     private static Gson gson = new Gson();
+    private static String newStoriesUrl = "https://hacker-news.firebaseio.com/v0/newstories.json";
+    private static String topStoriesUrl = "https://hacker-news.firebaseio.com/v0/topstories.json";
 
     @Override
     public String haeSuosituinUutinen() {
-        String suosituimmat = HTTPClient.callURL("https://hacker-news.firebaseio.com/v0/topstories.json");
-        suosituimmat = suosituimmat.replace("[", "");
-        suosituimmat = suosituimmat.replace("]", "");
-        String[] array = suosituimmat.split(",");
-        int suosituin = Integer.parseInt(array[0]);
-        String vastaus = HTTPClient.callURL("https://hacker-news.firebaseio.com/v0/item/" + suosituin + ".json?print=pretty");
-        Uutinen uutinen = gson.fromJson(vastaus, Uutinen.class);
+        Uutinen uutinen = getNewsStoryByFirstItemInJsonFile(topStoriesUrl);
         return "Suosituin uutinen on " + uutinen.toString();
     }
 
     @Override
     public String haeViimeisinUutinen() {
-        String uusimmat = HTTPClient.callURL("https://hacker-news.firebaseio.com/v0/newstories.json");
-        uusimmat = uusimmat.replace("[", "");
-        uusimmat = uusimmat.replace("]", "");
-        String[] array = uusimmat.split(",");
-        int viimeisin = Integer.parseInt(array[0]);
-        String vastaus = HTTPClient.callURL("https://hacker-news.firebaseio.com/v0/item/" + viimeisin + ".json?print=pretty");
-        Uutinen uutinen = gson.fromJson(vastaus, Uutinen.class);
+        Uutinen uutinen = getNewsStoryByFirstItemInJsonFile(newStoriesUrl);
         return "Viimeisin uutinen on " + uutinen.toString();
     }
+
+    private Uutinen getNewsStoryByFirstItemInJsonFile(final String url) throws JsonSyntaxException, NumberFormatException {
+        String jsonList = HTTPClient.callURL(url);
+        int newsItemId = getFirstIntegerInJsonList(jsonList);
+        System.out.println("");
+        return getNewsStoryById(newsItemId);
+    }
+
+    private int getFirstIntegerInJsonList(final String jsonList) throws NumberFormatException {
+        String[] array = parseJsonList(jsonList);
+        return Integer.parseInt(array[0]);
+    }
+
+    private String[] parseJsonList(String jsonString) {
+        jsonString = jsonString.replace("[", "");
+        jsonString = jsonString.replace("]", "");
+        System.out.println("");
+        return jsonString.split(",");
+    }
     
+    private Uutinen getNewsStoryById(final int newsId) throws JsonSyntaxException {
+        final String url = getNewsStoryUrl(newsId);
+        String vastaus = HTTPClient.callURL(url);
+        System.out.println("");
+        return gson.fromJson(vastaus, Uutinen.class);
+    }
+
+    private String getNewsStoryUrl(final int newsId) {
+        return "https://hacker-news.firebaseio.com/v0/item/" + newsId + ".json?print=pretty";
+    }
 }
