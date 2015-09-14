440ea3a25a54d9560f2dffa2121a2db8678c369b
Leo Honkanen <leo.o.honkanen@gmail.com>
2015-09-14 16:25:05 +0300

    refactor stream reading

--- src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
+++ src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
@@ -2,6 +2,7 @@
 package com.mycompany.hackernewsuutiset;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -22,16 +23,7 @@ public class HTTPClient {
                             urlConn.setReadTimeout(60 * 1000);
                         }   
 			if (urlConn != null && urlConn.getInputStream() != null) {
-				in = new InputStreamReader(urlConn.getInputStream(),
-						Charset.defaultCharset());
-				BufferedReader bufferedReader = new BufferedReader(in);
-				if (bufferedReader != null) {
-					int cp;
-					while ((cp = bufferedReader.read()) != -1) {
-						sb.append((char) cp);
-					}
-					bufferedReader.close();
-				}
+				in = readStream(in, urlConn, sb);
 			}
                 
             System.out.println("aaa");  
@@ -41,4 +33,18 @@ public class HTTPClient {
 		} 
 		return sb.toString();
 	}
+
+    private static InputStreamReader readStream(InputStreamReader in, URLConnection urlConn, StringBuilder sb) throws IOException {
+        in = new InputStreamReader(urlConn.getInputStream(),
+                Charset.defaultCharset());
+        BufferedReader bufferedReader = new BufferedReader(in);
+        if (bufferedReader != null) {
+            int cp;
+            while ((cp = bufferedReader.read()) != -1) {
+                sb.append((char) cp);
+            }
+            bufferedReader.close();
+        }
+        return in;
+    }
 }
