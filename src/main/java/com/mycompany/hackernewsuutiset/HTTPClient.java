5842c5d81851581208b9ee0eae20e88b26f24c1d
Janne Haapsaari <haaja@iki.fi>
2016-01-04 20:21:11 +0200

    Refactor callURL method

--- src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
+++ src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
@@ -2,7 +2,9 @@
 package com.mycompany.hackernewsuutiset;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
@@ -14,26 +16,37 @@ public class HTTPClient {
 		URLConnection urlConn = null;
 		InputStreamReader in = null;
 		try {
-			URL url = new URL(URLString);
-			urlConn = url.openConnection();
-			if (urlConn != null)
-				urlConn.setReadTimeout(60 * 1000);
-			if (urlConn != null && urlConn.getInputStream() != null) {
-				in = new InputStreamReader(urlConn.getInputStream(),
-						Charset.defaultCharset());
-				BufferedReader bufferedReader = new BufferedReader(in);
-				if (bufferedReader != null) {
-					int cp;
-					while ((cp = bufferedReader.read()) != -1) {
-						sb.append((char) cp);
-					}
-					bufferedReader.close();
-				}
-			}
+			urlConn = createUrlConnection(URLString);
+                        in = readFromUrlConnection(urlConn, in, sb);
 		in.close();
 		} catch (Exception e) {
 			throw new RuntimeException("Virhe hakiessa osoitteesta: "+ URLString, e);
 		} 
 		return sb.toString();
 	}
+
+    private static URLConnection createUrlConnection(String URLString) throws MalformedURLException, IOException {
+        URLConnection urlConn;
+        URL url = new URL(URLString);
+        urlConn = url.openConnection();
+        if (urlConn != null)
+            urlConn.setReadTimeout(60 * 1000);
+        return urlConn;
+    }
+
+    private static InputStreamReader readFromUrlConnection(URLConnection urlConn, InputStreamReader in, StringBuilder sb) throws IOException {
+        if (urlConn != null && urlConn.getInputStream() != null) {
+            in = new InputStreamReader(urlConn.getInputStream(),
+                    Charset.defaultCharset());
+            BufferedReader bufferedReader = new BufferedReader(in);
+            if (bufferedReader != null) {
+                int cp;
+                while ((cp = bufferedReader.read()) != -1) {
+                    sb.append((char) cp);
+                }
+                bufferedReader.close();
+            }
+        }
+        return in;
+    }
 }
